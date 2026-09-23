584f5a0ad5094e8fd4c63d1d2b6814fe3018a546
Débora Fraga <deborafragamoreira@gmail.com>
2026-09-23 19:32:54 -0300

    finaliza aula 7

--- src/main/java/com/example/imagemPecas/infra/repository/ImageRepository.java
+++ src/main/java/com/example/imagemPecas/infra/repository/ImageRepository.java
@@ -2,6 +2,9 @@ package com.example.imagemPecas.infra.repository;
 
 import com.example.imagemPecas.domain.entity.Image;
 import com.example.imagemPecas.domain.enums.ImageExtension;
+import jakarta.persistence.criteria.Root;
+import org.springframework.util.StringUtils;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -11,6 +14,25 @@ public interface ImageRepository extends JpaRepository<Image, String>,
         JpaSpecificationExecutor<Image> {
 
     default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
+        //SELECT * FROM IMAGE WHERE 1 =1
+        Specification<Image> conjunction = ((root, q, criteriaBuilder) -> criteriaBuilder.conjunction());
+        Specification<Image> spec = Specification.where(conjunction);
+
+        if(extension != null){
+            //AND EXTENSION = 'PNG'
+            Specification<Image> extensionEqual = ((root, q, cb) -> cb.equal(root.get("extension"), extension) );
+            spec = spec.and(extensionEqual);
+        }
+
+        if(StringUtils.hasText(query)){
+            // AND (NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY')
+            Specification<Image> nameLike = ((root, q, cb) -> cb.like( cb.upper(root.get("name")), "%" + query.toUpperCase() + " %"));
+            Specification<Image> tagsLike = ((root, q, cb) -> cb.like( cb.upper(root.get("tags")), "%" + query.toUpperCase() + "%"));
+
+            Specification<Image> nameOrTagsLike = Specification.anyOf(nameLike, tagsLike);
+
+            spec = spec.and(nameOrTagsLike);
+        }
         return findAll();
     }
 
