d768db0269ccd395a83e5018e4fe9d1df8e32db3
Débora Fraga <deborafragamoreira@gmail.com>
2026-09-23 22:28:32 -0300

    implementa pesquisa de imagens com Specifications e finaliza primeira parte do back-end

--- src/main/java/com/example/imagemPecas/application/images/ImagesController.java
+++ src/main/java/com/example/imagemPecas/application/images/ImagesController.java
@@ -1,5 +1,7 @@
 package com.example.imagemPecas.application.images;
 
+import com.example.imagemPecas.application.images.ImageDTO;
+import com.example.imagemPecas.application.images.ImageMapper;
 import com.example.imagemPecas.domain.entity.Image;
 import com.example.imagemPecas.domain.enums.ImageExtension;
 import com.example.imagemPecas.domain.service.ImageService;
@@ -7,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/images")
@@ -28,39 +30,71 @@ public class ImagesController {
 
     @PostMapping
     public ResponseEntity save(
-            @RequestParam("file")MultipartFile file,
+            @RequestParam("file") MultipartFile file,
             @RequestParam("name") String name,
             @RequestParam("tags") List<String> tags
-    ) throws IOException
-    {
-        log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-        Image image = mapper.mapToImage(file, name, tags);
-        Image savedImage = service.save(image);
+    ) throws IOException {
 
-        URI imageUri = buildImageURL(savedImage);
+        log.info("Imagem Recebida: name: {}, size: {}",
+                file.getOriginalFilename(), file.getSize());
+
+        Image image = mapper.mapToImage(file, name, tags);
+        Image saveImage = service.save(image);
+
+        URI imageUri = buildImageURL(saveImage);
+
         return ResponseEntity.created(imageUri).build();
     }
 
-    // /v1/images/{id}
     @GetMapping("{id}")
-    public ResponseEntity<byte[]> getImage(@PathVariable("id") String id){
+    public ResponseEntity<byte[]> getImage(@PathVariable("id") String id) {
+
         var possibleImage = service.getById(id);
-        if(possibleImage.isEmpty()){
+
+        if (possibleImage.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
         var image = possibleImage.get();
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(image.getExtension().getMediaType());
         headers.setContentLength(image.getSize());
-        headers.setContentDispositionFormData("inline; filename= \"" + image.getName()
-                + "\"",image.getFileName());
-        return new ResponseEntity<>(image.getFile(), headers, HttpStatus.OK);
+        headers.setContentDispositionFormData(
+                "inline; filename = \"" + image.getFileName() + "\"",
+                image.getFileName()
+        );
+
+        return new ResponseEntity<>(
+                image.getFile(),
+                headers,
+                HttpStatus.OK
+        );
     }
 
-    //localhost:8080/v1/images   /xzxzxzxzxzxzxzxzxzxz
-    private URI buildImageURL(Image image){
+    private URI buildImageURL(Image image) {
+
         String imagePath = "/" + image.getId();
-        return ServletUriComponentsBuilder.fromCurrentRequest().path(imagePath).build().toUri();
+
+        return ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path(imagePath)
+                .build()
+                .toUri();
+    }
+
+    // localhost:8080/v1/images?extension=PNG&query=Nature
+    @GetMapping
+    public ResponseEntity<List<ImageDTO>> search(
+            @RequestParam(value = "extension", required = false, defaultValue = "") String extension,
+            @RequestParam(value = "query", required = false) String query) {
+
+        var result = service.search(ImageExtension.ofName(extension), query);
+        var images = result.stream().map( image -> {
+            var url = buildImageURL(image);
+
+            return mapper.imageToDTO(image, url.toString());
+        }).collect(Collectors.toList());
+        return ResponseEntity.ok(images);
     }
 }
--- src/main/java/com/example/imagemPecas/infra/repository/ImageRepository.java
+++ src/main/java/com/example/imagemPecas/infra/repository/ImageRepository.java
@@ -1,39 +1,36 @@
 package com.example.imagemPecas.infra.repository;
 
+import ch.qos.logback.core.util.StringUtil;
 import com.example.imagemPecas.domain.entity.Image;
 import com.example.imagemPecas.domain.enums.ImageExtension;
-import jakarta.persistence.criteria.Root;
-import org.springframework.util.StringUtils;
+import com.example.imagemPecas.infra.repository.specs.GenericSpecs;
+import com.example.imagemPecas.infra.repository.specs.ImageSpecs;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.Locale;
 
-public interface ImageRepository extends JpaRepository<Image, String>,
-        JpaSpecificationExecutor<Image> {
+import static com.example.imagemPecas.infra.repository.specs.ImageSpecs.*;
+import static org.springframework.data.jpa.domain.Specification.anyOf;
+import static org.springframework.data.jpa.domain.Specification.where;
+
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image>{
 
     default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
-        //SELECT * FROM IMAGE WHERE 1 =1
-        Specification<Image> conjunction = ((root, q, criteriaBuilder) -> criteriaBuilder.conjunction());
-        Specification<Image> spec = Specification.where(conjunction);
+        //SELECT * FROM IMAGE WHERE 1 = 1
+        Specification<Image> spec = where(GenericSpecs.conjunction());
 
         if(extension != null){
             //AND EXTENSION = 'PNG'
-            Specification<Image> extensionEqual = ((root, q, cb) -> cb.equal(root.get("extension"), extension) );
-            spec = spec.and(extensionEqual);
+            spec = spec.and(extensionEqual(extension));
         }
 
         if(StringUtils.hasText(query)){
-            // AND (NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY')
-            Specification<Image> nameLike = ((root, q, cb) -> cb.like( cb.upper(root.get("name")), "%" + query.toUpperCase() + " %"));
-            Specification<Image> tagsLike = ((root, q, cb) -> cb.like( cb.upper(root.get("tags")), "%" + query.toUpperCase() + "%"));
-
-            Specification<Image> nameOrTagsLike = Specification.anyOf(nameLike, tagsLike);
-
-            spec = spec.and(nameOrTagsLike);
+            spec = spec.and( anyOf(nameLike(query), tagsLike(query)));
         }
-        return findAll();
+        return findAll(spec);
     }
-
 }
